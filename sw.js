62a8a3f800a55d7287c7020e911b9efe9df03c6d
nakadayuna184-lab <nakadayuna184@gmail.com>
2026-09-09 14:59:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='iphone-invader-v5';
+const CACHE='iphone-invader-v5-fixed';
 const FILES=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
